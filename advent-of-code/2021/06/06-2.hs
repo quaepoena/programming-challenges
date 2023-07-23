ebaf7376d5beed997cd12f7d4902c9e3d605c673
QP <qp@example.com>
2023-07-23 13:22:34 +0200

    removing prime marks

--- advent-of-code/2021/06/06-2.hs
+++ advent-of-code/2021/06/06-2.hs
@@ -4,17 +4,17 @@ import System.IO(readFile)
 import qualified Data.Map.Strict as Map
 
 memoizedFish :: Int -> Int
-memoizedFish = (map fish' [0 ..] !!)
-  where fish' 0 = 1
-        fish' 1 = 1
-        fish' 2 = 1
-        fish' 3 = 1
-        fish' 4 = 1
-        fish' 5 = 1
-        fish' 6 = 1
-        fish' 7 = 2
-        fish' 8 = 2
-        fish' n = memoizedFish (n - 7) + memoizedFish (n - 9)
+memoizedFish = (map fish [0 ..] !!)
+  where fish 0 = 1
+        fish 1 = 1
+        fish 2 = 1
+        fish 3 = 1
+        fish 4 = 1
+        fish 5 = 1
+        fish 6 = 1
+        fish 7 = 2
+        fish 8 = 2
+        fish n = memoizedFish (n - 7) + memoizedFish (n - 9)
 
 fishWithCounter :: Int -> Int -> Int
 fishWithCounter days counter = memoizedFish (days + (6 - counter))
