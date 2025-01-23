0698739aa5ca1b773ae667e62d9cce7fe47bdf4f
QP <quaepoena@example.com>
2025-01-23 17:02:10 +0100

    simplify fac

--- euler/020.hs
+++ euler/020.hs
@@ -1,7 +1,6 @@
 -- Simple factorial function.
 fac :: Integer -> Integer
-fac 0 = 1
-fac x = x * fac (x - 1)
+fac x = foldl1 (*) [1..x]
 
 -- Convert an integer to a list of its constituent digits.
 intToList :: Integer -> [Integer]
