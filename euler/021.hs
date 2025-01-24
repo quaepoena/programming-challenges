512a22a28cb9d10c445434da4820020a7296341f
QP <quaepoena@example.com>
2025-01-24 08:41:10 +0100

    add euler 021

---
+++ euler/021.hs
@@ -0,0 +1,22 @@
+amicDivisors :: Int -> [Int]
+amicDivisors n = amicDivisors' 1 n n
+
+-- Find divisors < n.
+amicDivisors' :: Int -> Int -> Int -> [Int]
+amicDivisors' x y n
+  | x >= y = []
+  | x == 1 = [x] ++ amicDivisors' (x + 1) (n `quot` x) n
+  | x ^ 2 == n = [x]
+  | n `mod` x == 0 = [x] ++ amicDivisors' (x + 1) (n `quot` x) n ++ [n `quot` x]
+  | otherwise = amicDivisors' (x + 1) y n
+
+amicPar :: Int -> Bool
+amicPar x =
+  let sumAmic z = sum $ amicDivisors z
+      sumX = sumAmic x
+      sumY = sumAmic sumX
+  in x == sumY && x /= sumX
+
+main :: IO ()
+main = do
+  print . sum $ filter amicPar [1..10000]
